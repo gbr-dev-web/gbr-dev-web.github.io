e0c7742a456902064f9b71eb097afec9e1a22e77
gabriel <gabrielsil20177@gmail.com>
2025-05-21 18:35:41 -0300

    feat: alteracoes iniciais, trocando dados, removendo elementos...

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,53 @@
-var gulp = require('gulp');
-var plumber = require('gulp-plumber');
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
 const uglify = require('gulp-uglify');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass')); // compatível com Dart Sass
 const wait = require('gulp-wait');
-const babel = require('gulp-babel');;
+const babel = require('gulp-babel');
 const rename = require('gulp-rename');
 
-gulp.task('scripts', function() {
-    return gulp.src('./js/scripts.js')
-        .pipe(plumber(plumber({
-            errorHandler: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        })))
-        .pipe(babel({
-          presets: [['@babel/env', {modules:false}]]
-        }))
-        .pipe(uglify({
-            output: {
-                comments: '/^!/'
-            }
-        }))
-        .pipe(rename({extname: '.min.js'}))
-        .pipe(gulp.dest('./js'));
-});
+// Tarefa para scripts JavaScript
+function scripts() {
+  return gulp.src('./js/scripts.js')
+    .pipe(plumber({
+      errorHandler: function (err) {
+        console.error(err);
+        this.emit('end');
+      }
+    }))
+    .pipe(babel({
+      presets: [['@babel/env', { modules: false }]]
+    }))
+    .pipe(uglify({
+      output: {
+        comments: /^!/
+      }
+    }))
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest('./js'));
+}
 
-gulp.task('styles', function () {
-    return gulp.src('./scss/styles.scss')
-        .pipe(wait(250))
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
-});
+// Tarefa para compilar SCSS
+function styles() {
+  return gulp.src('./scss/styles.scss')
+    .pipe(wait(250))
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+}
 
-gulp.task('watch', function() {
-    gulp.watch('./js/scripts.js', gulp.series('scripts'));
-    gulp.watch('./scss/styles.scss', gulp.series('styles'));
-});
+// Tarefa de observação
+function watchFiles() {
+  gulp.watch('./js/scripts.js', scripts);
+  gulp.watch('./scss/styles.scss', styles);
+}
+
+// Exportando tarefas
+exports.scripts = scripts;
+exports.styles = styles;
+exports.watch = watchFiles;
+
+// Tarefa padrão
+exports.default = gulp.series(
+  gulp.parallel(scripts, styles),
+  watchFiles
+);
